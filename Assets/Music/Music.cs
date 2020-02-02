3009966b6cfb1221354bb83740f94fee265b652d
GembaczkaM <m.gembaczka@hotmail.de>
2020-02-02 13:56:39 +0100

    Added Audiologic

---
+++ Assets/Music/Music.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioSource theParty;
+    public AudioSource theAftermath;
+    public AudioSource theIntro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SwitchToPast() 
+    {
+        theParty.Play(0);
+        Debug.Log("started");
+        theAftermath.Stop();
+        Debug.Log("stopped");
+    }
+    public void SwitchToPresent()
+    {
+        theAftermath.Play(0);
+        Debug.Log("started");
+        theParty.Stop();
+        Debug.Log("stopped");
+    }
+}
