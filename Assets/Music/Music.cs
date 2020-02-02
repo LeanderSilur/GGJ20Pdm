955f95b7c3efa40018228e1659db44ad0059803c
GembaczkaM <m.gembaczka@hotmail.de>
2020-02-02 14:55:15 +0100

    Modified Audiologic

--- Assets/Music/Music.cs
+++ Assets/Music/Music.cs
@@ -7,6 +7,8 @@ public class Music : MonoBehaviour
     public AudioSource theParty;
     public AudioSource theAftermath;
     public AudioSource theIntro;
+    private int TimeAftermathMusic;
+    private int TimePartyMusic;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,16 +22,24 @@ public class Music : MonoBehaviour
     }
     public void SwitchToPast() 
     {
-        theParty.Play(0);
+        Debug.Log(TimePartyMusic);
+        theParty.Play();
+        theParty.timeSamples = TimePartyMusic;
         Debug.Log("started");
+        TimeAftermathMusic = theAftermath.timeSamples;
         theAftermath.Stop();
         Debug.Log("stopped");
+
     }
     public void SwitchToPresent()
     {
-        theAftermath.Play(0);
+        theAftermath.Play();
+        theAftermath.timeSamples = TimeAftermathMusic;
         Debug.Log("started");
+        TimePartyMusic = theParty.timeSamples;
         theParty.Stop();
         Debug.Log("stopped");
+
+       
     }
 }
