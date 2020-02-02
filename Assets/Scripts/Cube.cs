14526723f7b7d0678a1258921a8bd6d7c880de0b
André Eckner <ae@mbp-ae.local>
2020-02-02 11:50:35 +0100

    Graphical Frontend and Character Interactions

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : NPC
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		Name = "Cubi";
+	} 
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public override IEnumerator Interact(int actionID)
+	{
+		yield return base.Interact(-1);
+	}
+}
